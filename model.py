5725e0402b6f6f9e9c750415dadae3a5b8dbb69d
shot star <52364706+bruno686@users.noreply.github.com>
2024-05-26 15:07:04 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F 
+import numpy as np
 
 
 class NCF(nn.Module):
@@ -110,5 +111,427 @@ class NCF(nn.Module):
 
 		prediction = self.predict_layer(concat)
   
-
+		# # 为了适配PLC，在这里加sigmoid
+		# prediction = torch.sigmoid(prediction)
 		return prediction.view(-1)
+
+def apply_activation(act_name, x):
+    """
+    Apply activation function
+    :param act_name: name of the activation function
+    :param x: input
+    :return: output after activation
+    """
+    if act_name == 'sigmoid':
+        return torch.sigmoid(x)
+    elif act_name == 'tanh':
+        return torch.tanh(x)
+    elif act_name == 'relu':
+        return torch.relu(x)
+    elif act_name == 'elu':
+        return F.elu(x)
+    else:
+        raise NotImplementedError('Choose appropriate activation function. (current input: %s)' % act_name)
+
+
+class CDAE(nn.Module):
+    """
+    Collaborative Denoising Autoencoder model class
+    """
+    def __init__(self, num_users, num_items, hidden_dim=32, device="cuda",
+                 corruption_ratio=0.5, act='tanh'):
+        """
+        :param model_conf: model configuration
+        :param num_users: number of users
+        :param num_items: number of items
+        :param device: choice of device
+        """
+        super(CDAE, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.corruption_ratio = corruption_ratio
+        self.num_users = num_users
+        self.num_items = num_items
+        self.device = device
+        self.act = act
+
+        self.user_embedding = nn.Embedding(self.num_users, self.hidden_dim)
+        self.encoder = nn.Linear(self.num_items, self.hidden_dim)
+        self.decoder = nn.Linear(self.hidden_dim, self.num_items)
+
+    def forward(self, user_id, rating_matrix):
+        """
+        Forward pass
+        :param rating_matrix: rating matrix
+        """
+        # normalize the rating matrix
+        user_degree = torch.norm(rating_matrix, 2, 1).reshape(-1, 1)  # user, 1
+        item_degree = torch.norm(rating_matrix, 2, 0).reshape(1, -1)  # 1, item
+        normalize = torch.sqrt(user_degree @ item_degree)
+        zero_mask = normalize == 0
+        normalize = torch.masked_fill(normalize, zero_mask.bool(), 1e-10)
+
+        normalized_rating_matrix = rating_matrix / normalize
+
+        # corrupt the rating matrix
+        normalized_rating_matrix = F.dropout(normalized_rating_matrix, self.corruption_ratio, training=self.training)
+
+        # build the collaborative denoising autoencoder
+        enc = self.encoder(normalized_rating_matrix) + self.user_embedding(user_id)
+        enc = apply_activation(self.act, enc)
+        dec = self.decoder(enc)
+
+        return torch.sigmoid(dec)
+
+
+class BasicModel(nn.Module):
+    def __init__(self):
+        super(BasicModel, self).__init__()
+
+    def getUsersRating(self, users):
+        raise NotImplementedError
+
+
+class PairWiseModel(BasicModel):
+    def __init__(self):
+        super(PairWiseModel, self).__init__()
+
+    def bpr_loss(self, users, pos, neg):
+        """
+        Parameters:
+            users: users list
+            pos: positive items for corresponding users
+            neg: negative items for corresponding users
+        Return:
+            (log-loss, l2-loss)
+        """
+        raise NotImplementedError
+
+class LightGCN(BasicModel):
+    def __init__(self, user_num, item_num,
+                 norm_adj,
+                 latent_dim=64,
+                 n_layers=3,
+                 keep_prob=0.6,
+                 A_split=False,
+                 dropout=0,
+                 pretrain=0,
+                 device='cuda'
+                 ):
+        super(LightGCN, self).__init__()
+        self.latent_dim = latent_dim
+        self.n_layers = n_layers
+        self.keep_prob = keep_prob
+        self.A_split = A_split
+        self.num_users = user_num
+        self.num_items = item_num
+        self.dropout = dropout
+        self.pretrain = pretrain
+
+        self.Graph = self._convert_sp_mat_to_sp_tensor(norm_adj)
+        self.Graph = self.Graph.coalesce().to(device)
+        self._init_weight_()
+
+    def _init_weight_(self):
+        self.embedding_user = torch.nn.Embedding(
+            num_embeddings=self.num_users, embedding_dim=self.latent_dim)
+        self.embedding_item = torch.nn.Embedding(
+            num_embeddings=self.num_items, embedding_dim=self.latent_dim)
+        if self.pretrain == 0:
+            #             nn.init.xavier_uniform_(self.embedding_user.weight, gain=1)
+            #             nn.init.xavier_uniform_(self.embedding_item.weight, gain=1)
+            #             print('use xavier initilizer')
+            # random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
+            nn.init.normal_(self.embedding_user.weight, std=0.1)
+            nn.init.normal_(self.embedding_item.weight, std=0.1)
+            # world.cprint('use NORMAL distribution initilizer')
+        else:
+            self.embedding_user.weight.data.copy_(torch.from_numpy(self.config['user_emb']))
+            self.embedding_item.weight.data.copy_(torch.from_numpy(self.config['item_emb']))
+            print('use pretarined data')
+        self.f = nn.Sigmoid()
+        print(f"lgn is ready to go(dropout:{self.dropout})")
+
+        # print("save_txt")
+
+    def _convert_sp_mat_to_sp_tensor(self, X):
+        coo = X.tocoo().astype(np.float32)
+        row = torch.Tensor(coo.row).long()
+        col = torch.Tensor(coo.col).long()
+        index = torch.stack([row, col])
+        data = torch.FloatTensor(coo.data)
+        return torch.sparse.FloatTensor(index, data, torch.Size(coo.shape))
+
+    def __dropout_x(self, x, keep_prob):
+        size = x.size()
+        index = x.indices().t()
+        values = x.values()
+        random_index = torch.rand(len(values)) + keep_prob
+        random_index = random_index.int().bool()
+        index = index[random_index]
+        values = values[random_index] / keep_prob
+        g = torch.sparse.FloatTensor(index.t(), values, size)
+        return g
+
+    def __dropout(self, keep_prob):
+        if self.A_split:
+            graph = []
+            for g in self.Graph:
+                graph.append(self.__dropout_x(g, keep_prob))
+        else:
+            graph = self.__dropout_x(self.Graph, keep_prob)
+        return graph
+
+    def computer(self):
+        """
+        propagate methods for lightGCN
+        """
+        users_emb = self.embedding_user.weight
+        items_emb = self.embedding_item.weight
+        all_emb = torch.cat([users_emb, items_emb])
+        #   torch.split(all_emb , [self.num_users, self.num_items])
+        embs = [all_emb]
+        if self.dropout:
+            if self.training:
+                g_droped = self.__dropout(self.keep_prob)
+            else:
+                g_droped = self.Graph
+        else:
+            g_droped = self.Graph
+
+        for layer in range(self.n_layers):
+            if self.A_split:
+                temp_emb = []
+                for f in range(len(g_droped)):
+                    temp_emb.append(torch.sparse.mm(g_droped[f], all_emb))
+                side_emb = torch.cat(temp_emb, dim=0)
+                all_emb = side_emb
+            else:
+                all_emb = torch.sparse.mm(g_droped, all_emb)
+            embs.append(all_emb)
+        embs = torch.stack(embs, dim=1)
+        # print(embs.size())
+        light_out = torch.mean(embs, dim=1)
+        users, items = torch.split(light_out, [self.num_users, self.num_items])
+        return users, items
+
+    def getUsersRating(self, users):
+        all_users, all_items = self.computer()
+        users_emb = all_users[users.long()]
+        items_emb = all_items
+        rating = self.f(torch.matmul(users_emb, items_emb.t()))
+        return rating
+
+    def getEmbedding(self, users, pos_items, neg_items):
+        all_users, all_items = self.computer()
+        users_emb = all_users[users]
+        pos_emb = all_items[pos_items]
+        neg_emb = all_items[neg_items]
+        users_emb_ego = self.embedding_user(users)
+        pos_emb_ego = self.embedding_item(pos_items)
+        neg_emb_ego = self.embedding_item(neg_items)
+        return users_emb, pos_emb, neg_emb, users_emb_ego, pos_emb_ego, neg_emb_ego
+
+    def bpr_loss(self, users, pos, neg):
+        (users_emb, pos_emb, neg_emb,
+         userEmb0, posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
+        reg_loss = (1 / 2) * (userEmb0.norm(2).pow(2) +
+                              posEmb0.norm(2).pow(2) +
+                              negEmb0.norm(2).pow(2)) / float(len(users))
+        pos_scores = torch.mul(users_emb, pos_emb)
+        pos_scores = torch.sum(pos_scores, dim=1)
+        neg_scores = torch.mul(users_emb, neg_emb)
+        neg_scores = torch.sum(neg_scores, dim=1)
+
+        loss = torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
+
+        return loss, reg_loss
+
+
+    def reg_loss(self, users, pos, neg):
+        (users_emb, pos_emb, neg_emb,
+         userEmb0, posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
+        reg_loss = (1 / 2) * (userEmb0.norm(2).pow(2) +
+                              posEmb0.norm(2).pow(2) +
+                              negEmb0.norm(2).pow(2)) / float(len(users))
+        return reg_loss
+
+    def forward(self, users, items):
+        # compute embedding
+        all_users, all_items = self.computer()
+        # print('forward')
+        # all_users, all_items = self.computer()
+        users_emb = all_users[users]
+        items_emb = all_items[items]
+        inner_pro = torch.mul(users_emb, items_emb)
+        gamma = torch.sum(inner_pro, dim=1)
+        # return torch.sigmoid(gamma)
+        return gamma
+
+class NGCF(BasicModel):
+    def __init__(self, user_num, item_num,
+                 norm_adj,
+                 latent_dim=64,
+                 n_layers=3,
+                 keep_prob=0.6,
+                 A_split=False,
+                 dropout=0,
+                 pretrain=0,
+                 device='cuda'
+                 ):
+        super(NGCF, self).__init__()
+        self.latent_dim = latent_dim
+        self.n_layers = n_layers
+        self.keep_prob = keep_prob
+        self.A_split = A_split
+        self.num_users = user_num
+        self.num_items = item_num
+        self.dropout = dropout
+        self.pretrain = pretrain
+        self.dropout = torch.nn.Dropout(0.2)
+
+        self.Graph = self._convert_sp_mat_to_sp_tensor(norm_adj)
+        self.Graph = self.Graph.coalesce().to(device)
+        self._init_weight_()
+
+    def _init_weight_(self):
+        self.embedding_user = torch.nn.Embedding(
+            num_embeddings=self.num_users, embedding_dim=self.latent_dim)
+        self.embedding_item = torch.nn.Embedding(
+            num_embeddings=self.num_items, embedding_dim=self.latent_dim)
+        if self.pretrain == 0:
+            #             nn.init.xavier_uniform_(self.embedding_user.weight, gain=1)
+            #             nn.init.xavier_uniform_(self.embedding_item.weight, gain=1)
+            #             print('use xavier initilizer')
+            # random normal init seems to be a better choice when lightGCN actually don't use any non-linear activation function
+            nn.init.normal_(self.embedding_user.weight, std=0.1)
+            nn.init.normal_(self.embedding_item.weight, std=0.1)
+            # world.cprint('use NORMAL distribution initilizer')
+        else:
+            self.embedding_user.weight.data.copy_(torch.from_numpy(self.config['user_emb']))
+            self.embedding_item.weight.data.copy_(torch.from_numpy(self.config['item_emb']))
+            print('use pretarined data')
+        self.f = nn.Sigmoid()
+        print(f"lgn is ready to go(dropout:{self.dropout})")
+
+        # print("save_txt")
+
+    def _convert_sp_mat_to_sp_tensor(self, X):
+        coo = X.tocoo().astype(np.float32)
+        row = torch.Tensor(coo.row).long()
+        col = torch.Tensor(coo.col).long()
+        index = torch.stack([row, col])
+        data = torch.FloatTensor(coo.data)
+        return torch.sparse.FloatTensor(index, data, torch.Size(coo.shape))
+
+    def __dropout_x(self, x, keep_prob):
+        size = x.size()
+        index = x.indices().t()
+        values = x.values()
+        random_index = torch.rand(len(values)) + keep_prob
+        random_index = random_index.int().bool()
+        index = index[random_index]
+        values = values[random_index] / keep_prob
+        g = torch.sparse.FloatTensor(index.t(), values, size)
+        return g
+
+    def __dropout(self, keep_prob):
+        if self.A_split:
+            graph = []
+            for g in self.Graph:
+                graph.append(self.__dropout_x(g, keep_prob))
+        else:
+            graph = self.__dropout_x(self.Graph, keep_prob)
+        return graph
+
+    def computer(self):
+        """
+        propagate methods for lightGCN
+        """
+        users_emb = self.embedding_user.weight
+        items_emb = self.embedding_item.weight
+        all_emb = torch.cat([users_emb, items_emb])
+        #   torch.split(all_emb , [self.num_users, self.num_items])
+        embs = [all_emb]
+        if self.dropout:
+            if self.training:
+                g_droped = self.__dropout(self.keep_prob)
+            else:
+                g_droped = self.Graph
+        else:
+            g_droped = self.Graph
+
+        for layer in range(self.n_layers):
+            if self.A_split:
+                temp_emb = []
+                for f in range(len(g_droped)):
+                    # 应用 leaky relu 激活函数
+                    activated = F.leaky_relu(torch.sparse.mm(g_droped[f], all_emb), negative_slope=0.2)
+                    # 应用 dropout
+                    dropped_out = self.dropout(activated)
+                    # 归一化
+                    normalized = F.normalize(dropped_out, p=2, dim=1)
+                    temp_emb.append(normalized)
+                side_emb = torch.cat(temp_emb, dim=0)
+                all_emb = side_emb
+            else:
+                all_emb = torch.sparse.mm(g_droped, all_emb)
+            embs.append(all_emb)
+        embs = torch.stack(embs, dim=1)
+        # print(embs.size())
+        light_out = torch.mean(embs, dim=1)
+        users, items = torch.split(light_out, [self.num_users, self.num_items])
+        return users, items
+
+    def getUsersRating(self, users):
+        all_users, all_items = self.computer()
+        users_emb = all_users[users.long()]
+        items_emb = all_items
+        rating = self.f(torch.matmul(users_emb, items_emb.t()))
+        return rating
+
+    def getEmbedding(self, users, pos_items, neg_items):
+        all_users, all_items = self.computer()
+        users_emb = all_users[users]
+        pos_emb = all_items[pos_items]
+        neg_emb = all_items[neg_items]
+        users_emb_ego = self.embedding_user(users)
+        pos_emb_ego = self.embedding_item(pos_items)
+        neg_emb_ego = self.embedding_item(neg_items)
+        return users_emb, pos_emb, neg_emb, users_emb_ego, pos_emb_ego, neg_emb_ego
+
+    def bpr_loss(self, users, pos, neg):
+        (users_emb, pos_emb, neg_emb,
+         userEmb0, posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
+        reg_loss = (1 / 2) * (userEmb0.norm(2).pow(2) +
+                              posEmb0.norm(2).pow(2) +
+                              negEmb0.norm(2).pow(2)) / float(len(users))
+        pos_scores = torch.mul(users_emb, pos_emb)
+        pos_scores = torch.sum(pos_scores, dim=1)
+        neg_scores = torch.mul(users_emb, neg_emb)
+        neg_scores = torch.sum(neg_scores, dim=1)
+
+        loss = torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
+
+        return loss, reg_loss
+
+
+    def reg_loss(self, users, pos, neg):
+        (users_emb, pos_emb, neg_emb,
+         userEmb0, posEmb0, negEmb0) = self.getEmbedding(users.long(), pos.long(), neg.long())
+        reg_loss = (1 / 2) * (userEmb0.norm(2).pow(2) +
+                              posEmb0.norm(2).pow(2) +
+                              negEmb0.norm(2).pow(2)) / float(len(users))
+        return reg_loss
+
+    def forward(self, users, items):
+        # compute embedding
+        all_users, all_items = self.computer()
+        # print('forward')
+        # all_users, all_items = self.computer()
+        users_emb = all_users[users]
+        items_emb = all_items[items]
+        inner_pro = torch.mul(users_emb, items_emb)
+        gamma = torch.sum(inner_pro, dim=1)
+        # return torch.sigmoid(gamma)
+        return gamma
+
